6cf14c4d689b7816a8cf0598fe036e22460132a3
szutsattila <szuts.attila97@gmail.com>
2021-02-21 14:19:47 +0100

    add cakes page

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
+@app.route('/cakes')
+def cakes():
+    return 'Yummy cakes!'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
