3fca26e2da3d7c8267f9d7e4a86cbc79e7c05794
szutsattila <szuts.attila97@gmail.com>
2021-02-21 14:46:17 +0100

    add dynamic content

--- main.py
+++ main.py
@@ -10,5 +10,9 @@ def index():
 def cakes():
     return render_template('cakes.html')
 
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
