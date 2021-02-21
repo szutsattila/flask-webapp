72c7f37a45ce2b0da16a5745e660484785e93430
szutsattila <szuts.attila97@gmail.com>
2021-02-21 14:34:12 +0100

    add cakes.html template

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/cakes')
 def cakes():
-    return 'Yummy cakes!'
+    return render_template('cakes.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
